61cb1abc320d83d0267c10d2c9407518a08f111f
toosyou <toosyou@mip1070.mip1070.nctucs.net>
2016-11-24 02:07:12 +0800

    make iteration cleaner

--- NeuralSeg.py
+++ NeuralSeg.py
@@ -3,6 +3,7 @@ import os
 import progressbar as pb
 import numpy as np
 import random
+from itertools import product
 import mtp
 import tflearn
 from tflearn.layers.core import input_data, dropout, fully_connected
@@ -40,13 +41,13 @@ def get_data( in_mtp, index_start, size, balance=False):
     X = list()
     Y = list()
     number_tip_block = 0
+    # change directory to ams
+    original_directory = os.getcwd()
+    os.chdir(DIRECTORY_AMS)
     for i, points in enumerate(in_mtp[index_start: index_start+size]):
         am_name = get_am_filename(points.name)
         # read neural raw data
-        original_directory = os.getcwd()
-        os.chdir(DIRECTORY_AMS)
         raw = neuron.NeuronRaw(am_name)
-        os.chdir(original_directory)
         if raw.valid == False: # cannot read from am_name
             print("*****ERROR READING: ", am_name, " *****", file=sys.stderr)
             continue
@@ -54,29 +55,30 @@ def get_data( in_mtp, index_start, size, balance=False):
         print("Blocklizing ", i, " of ", size, " ",am_name, " :")
         # blocklize with step = (8, 8, 8)
         pbar_block = pb.ProgressBar()
-        for x in pbar_block(range(-8, raw.size[0]+8, 8)):
-            for y in range(-8, raw.size[1]+8, 8):
-                for z in range(-8, raw.size[2]+8, 8):
-                    raw_block = raw.block([x, y, z])
-                    if not raw_block.is_empty():
-                        raw_block = raw_block.normalize() # normalize to (-1, 1)
-                        X.append( raw_block.intensity.reshape( (16, 16, 16, 1) ) )
-                        # check if any tips is in the center of raw block
-                        find_tips = False
-                        for coor in points.coordinates:
-                            if raw_block.is_in_the_center(coor):
-                                find_tips = True
-                                break
-                        if find_tips:
+        for x, y, z in pbar_block( product( range(-8, raw.size[0]+8, 8), range(-8, raw.size[1]+8, 8), range(-8, raw.size[2]+8, 8)) ):
+            raw_block = raw.block([x, y, z])
+            if not raw_block.is_empty():
+                raw_block = raw_block.normalize(rg=[-1, 1])
+                X.append( raw_block.intensity.reshape( (16, 16, 16, 1) ) ) # normalize to (-1, 1)
+                # check if any tips is in the center of raw block
+                if raw_block.points_in_the_center(points.coordinates):
+                    Y.append(1)
+                    # make more positive data using mirroring
+                    for x, y, z in product( (0, 1), repeat=3 ):
+                        if (x, y, z) != (0, 0, 0):
+                            X.append( raw_block.mirror([x, y, z]).intensity.reshape(16, 16, 16, 1) )
                             Y.append(1)
-                            number_tip_block += 1
-                        else:
-                            Y.append(0)
+                    number_tip_block += 8
+                else:
+                    Y.append(0)
         print("X size: ", len(X))
-        print("number of tip block: ", number_tip_block)
+        print("# of non-tip block: ", len(X) - number_tip_block)
+        print("# of tip block: ", number_tip_block)
 
     X = np.array(X)
     Y = np.array(tflearn.data_utils.to_categorical(Y, 2) )
+    # change directory back
+    os.chdir(original_directory)
     return X, Y
 
 if __name__ == '__main__':
